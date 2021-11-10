6272df5f1f04ac9be2569dab24ffd53542a8cd86
JackyCCC <jacky9813@hotmail.com>
2021-11-10 20:28:05 +0800

    template.sh: Using case instead of ifs, splitter added, other adjustments

--- template.sh
+++ template.sh
@@ -4,6 +4,13 @@ PROJECT=$(gcloud config list project 2>/dev/null | grep = | sed "s/^[^=]*= //")
 # Leave this variable blank if you want to enable doing all tasks at once
 DISABLE_ALL_TASK=1
 
+splitter(){
+    i=$(tput cols)
+    while [ $i -gt "0" ]; do
+    	echo -n =
+        i=$(($i - 1))
+    done
+}
 pause(){
 	read -p "Press Enter to continue"
 }
@@ -14,34 +21,63 @@ check_return(){
 	fi
 }
 echo_cmd(){
-	echo ================================================================================
+    splitter
 	echo $@
 	$@
 }
 
+# TODO: PUT THE TASKS HERE
 
-if [ "$PROJECT" == "" ]; then
-	echo "Warning: No selected project"
-	echo "You can still proceed to execute anyway or Ctrl-C to exit"
-else
-	echo "Please confirm your target project is $PROJECT"
-fi
-pause
-if [ "$1" == "all" ]; then
-	if [ $DISABLE_ALL_TASK ] ; then
-		echo "Doing all tasks at once has been disabled"
-		exit 1
-	else
-		echo "No argument will be able to passed to any task."
-		echo "Are you sure you wanna do all at once?"
-		pause
-		task=1
-		while [[ $(type -t task$task) == function ]]; do
-			task$task
-			task=$(($task + 1))
-		done
-	fi
-else
-	[[ $(type -t task$1) == function ]] && task$1 $@ || echo "No task named task$1"
-fi
+case "$1" in
+    "all")
+    	if [ $DISABLE_ALL_TASK ] ; then
+    		echo "Doing all tasks at once has been disabled"
+    		exit 1
+    	else
+            if [ "$PROJECT" == "" ]; then
+            	echo "Warning: No selected project"
+            	echo "You can still proceed to execute anyway or press Ctrl-C to exit"
+            else
+            	echo "Please confirm your target project is $PROJECT"
+            fi
+            pause
+    		echo "No argument will be able to passed to any task."
+    		echo "Are you sure you wanna do all at once?"
+    		pause
+    		task=${START_TASK:-1}
+    		while [[ $(type -t task$task) == function ]]; do
+                splitter
+    			task$task
+    			task=$(($task + 1))
+    		done
+    	fi
+        ;;
+    "help" | "")
+        cat << EOF
+Usage:
+    $0 <task_number>
+        Execute specified task.
+
+    $0 all
+        Execute all tasks from task 1.
+
+    START_TASK=<task_number> $0 all
+        Execute tasks start from specified task.
+
+    $0 help
+        Display this message
+EOF
+        ;;
+    *)
+        if [ "$PROJECT" == "" ]; then
+        	echo "Warning: No selected project"
+        	echo "You can still proceed to execute anyway or Ctrl-C to exit"
+        else
+        	echo "Please confirm your target project is $PROJECT"
+        fi
+        pause
+        splitter
+	    [[ $(type -t task$1) == function ]] && task$1 $@ || echo "No task named task$1"
+        ;;
+esac
 
