51639fd817ed0cad9e7f5cc8734b0659612be880
JackyCCC <jacky9813@hotmail.com>
2021-11-12 09:50:30 +0800

    GSP051: new course, task 1 implemented

---
+++ GSP0xx/GSP051-ContinuousDeliveryWithJenkinsInKubernetesEngine.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+PROJECT=$(gcloud config list project 2>/dev/null | grep = | sed "s/^[^=]*= //")
+CLUSTER_NAME=jenkins-cd
+# Leave this variable blank if you want to enable doing all tasks at once
+DISABLE_ALL_TASK=1
+
+splitter(){
+    i=$(tput cols)
+    while [ $i -gt "0" ]; do
+    	echo -n =
+        i=$(($i - 1))
+    done
+}
+pause(){
+	read -p "Press Enter to continue"
+}
+check_return(){
+	if [ $1 -ne "0" ]; then
+		echo "Error detected"
+		pause
+	fi
+}
+echo_cmd(){
+    splitter
+    echo $@
+    eval $@
+    local RET=$?
+    if [ $RET -ne "0" ]; then
+        echo "Error occured! You can ignore this message and press enter to continue."
+        pause
+    fi
+    return $RET
+}
+
+task1(){
+ORIGINAL_WD=$(realpath $(pwd))
+cat << EOF
+Task 1 - Preparation
+This task covers the following sections in GSP051:
+* Setup
+* Clone the repository
+* Provisioning Jenkins
+* Setup Helm
+* Configure and Install Jenkins
+* Connect to Jenkins
+EOF
+pause
+
+# Downloading repository and create a new k8s cluster
+echo_cmd gcloud config set compute/zone us-east1-d
+echo_cmd git clone https://github.com/GoogleCloudPlatform/continuous-deployment-on-kubernetes.git
+echo_cmd cp $(realpath $0) continuous-deployment-on-kubernetes/
+echo_cmd gcloud container clusters create $CLUSTER_NAME --num-nodes=2 --machine-type=n1-standard-2 --scopes="https://www.googleapis.com/auth/source.read_write,cloud-platform"
+echo_cmd gcloud container clousters get-credentials $CLUSTER_NAME
+echo_cmd kubectl cluster-info
+splitter
+echo "Checkpoint reached"
+pause
+
+# Install Jenkins to k8s cluster
+echo_cmd helm repo add jenkins https://charts.jenkins.io
+echo_cmd helm repo update
+splitter
+echo "** Warning: Changing directory to continuous-deployment-on-kubernetes"
+cd continuous-deployment-on-kubernetes
+echo_cmd gsutil cp gs://spls/gsp330/values.yaml jenkins/values.yaml
+echo_cmd helm install cd jenkins/jenkins -f jenkins/values.yaml --wait
+echo_cmd kubectl get pods
+splitter
+echo "Checkpoint reached"
+pause
+
+echo_cmd kubectl create clusterrolebinding jenkins-deploy --clusterrole=cluster-admin --serviceaccount=default:cd-jenkins
+
+splitter
+echo "Waiting Jenkins to be ready"
+IP_ADDRESS=$(kubectl get services | grep LoadBalancer | grep jenkins | awk '{print $4}')
+while [ "$IP_ADDRESS" == "<pending>" ]; do
+sleep 1
+IP_ADDRESS=$(kubectl get services | grep LoadBalancer | grep jenkins | awk '{print $4}')
+done
+sleep 10
+echo_cmd kubectl get services
+
+echo_cmd 'printf $(kubectl get secret cd-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo'
+PASS=$(kubectl get secret cd-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode)
+POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/component=jenkins-master" -l "app.kubernetes.io/instance=cd" -o jsonpath="{.items[0].metadata.name}")
+splitter
+cat << EOF
+Connect to jenkins:
+link: http://$IP_ADDRESS:8080
+username: admin
+password: $PASS
+
+Or run "kubectl port-forward $POD_NAME 8080:8080" in another cloud console and click
+"Web Preview" => "Preview on port 8080", then type in the same credential listed 
+above.
+But if you use this method, do not close that console before you've done all the
+tasks.
+EOF
+
+splitter
+echo "** Warning: Changing directory back"
+cd $ORIGINAL_WD
+} # End of task 1
+
+task2(){
+
+}
+
+case "$1" in
+    "all")
+    	if [ $DISABLE_ALL_TASK ] ; then
+    		echo "Doing all tasks at once has been disabled"
+    		exit 1
+    	else
+            if [ "$PROJECT" == "" ]; then
+            	echo "Warning: No selected project"
+            	echo "You can still proceed to execute anyway or press Ctrl-C to exit"
+            else
+            	echo "Please confirm your target project is $PROJECT"
+            fi
+            pause
+    		echo "No argument will be able to passed to any task."
+    		echo "Are you sure you wanna do all at once?"
+    		pause
+    		task=${START_TASK:-1}
+    		while [[ $(type -t task$task) == function ]]; do
+                splitter
+    			task$task
+    			task=$(($task + 1))
+    		done
+    	fi
+        ;;
+    "help" | "")
+        cat << EOF
+Usage:
+    $0 <task_number>
+        Execute specified task.
+
+    $0 all
+        Execute all tasks from task 1.
+
+    START_TASK=<task_number> $0 all
+        Execute tasks start from specified task.
+
+    $0 help
+        Display this message
+EOF
+        ;;
+    *)
+        if [ "$PROJECT" == "" ]; then
+        	echo "Warning: No selected project"
+        	echo "You can still proceed to execute anyway or Ctrl-C to exit"
+        else
+        	echo "Please confirm your target project is $PROJECT"
+        fi
+        pause
+        splitter
+	    [[ $(type -t task$1) == function ]] && task$1 $@ || echo "No task named task$1"
+        ;;
+esac
+
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ These scripts may not work since I may still working on them.
 ### Available scripts
 * [GSP007 - Set up Network and HTTP Load Balancers](GSP0xx/GSP007-SetUpNetworkAndHTTPLoadBalancers.sh)
 * (Tested)[GSP021 - Orchestrating the Cloud with Kubernetes](GSP0xx/GSP021-OrchestratingTheCloudWithKubernetes.sh)
+* [GSP051 - Continuous Delivery with Jenkins in Kubernetes Engine](GSP0xx/GSP051-ContinuousDeliveryWithJenkinsInKubernetesEngine.sh)
 * (Tested)[GSP053 - Managing Deployments Using Kubernetes Engine](GSP0xx/GSP053-ManagingDeploymentsUsingKubernetesEngine.sh)
 * [GSP100 - Kubernetes Engines: Qwik Start](GSP1xx/GSP100-KubernetesEngines_QwikStart.sh)
 * (Tested)[GSP117 - Setting up Jenkins on Kubernetes Engine](GSP1xx/GSP117-SettingUpJenkinsOnKubernetesEngine.sh)
